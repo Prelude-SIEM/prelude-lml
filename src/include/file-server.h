3927982e465fe146461d5e2ccffe8e486e07f18a
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2002-04-08 08:52:06 +0000

    *** empty log message ***
    git-svn-id: file:///home/yoann/dev/prelude/git/nok/SVN/trunk/prelude-lml@972 09c5ec92-17d4-0310-903a-819935f44dba

--- src/include/file-server.h
+++ src/include/file-server.h
@@ -1,3 +1,26 @@
+/*****
+*
+* Copyright (C) 1998 - 2002 Yoann Vandoorselaere <yoann@mandrakesoft.com>
+* All Rights Reserved
+*
+* This file is part of the Prelude program.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by 
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING.  If not, write to
+* the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*****/
+
 int file_server_wake_up(regex_list_t *list, queue_t *queue);
 int file_server_monitor_file(const char *file, int fd);
 int file_server_standalone(regex_list_t *list, queue_t *queue);
