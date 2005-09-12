d5a6037badc0cc14417d0917e5ba07c703db4712
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-09-12 14:41:38 +0000

    ditto.
    git-svn-id: file:///home/yoann/dev/prelude/git/nok/SVN/trunk/prelude-lml@7169 09c5ec92-17d4-0310-903a-819935f44dba

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,8 @@
+GNULIB_TOOL=`which gnulib-tool 2> /dev/null`
+if test -n "$GNULIB_TOOL"; then
+        $GNULIB_TOOL --import > /dev/null
+fi
+
 autoreconf --install --force
 echo
 echo "You can now run \"./configure --enable-developer-mode\" and \"make\""
