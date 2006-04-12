e84c89cc160dcde170b703a0249f0bfc2613b1ef
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2006-04-12 10:37:04 +0000

    Missing copyright notice.
    git-svn-id: file:///home/yoann/dev/prelude/git/nok/SVN/trunk/prelude-lml@8136 09c5ec92-17d4-0310-903a-819935f44dba

--- plugins/pcre/value-container.h
+++ plugins/pcre/value-container.h
@@ -1,5 +1,28 @@
-#ifndef VALUE_H
-#define VALUE_H
+/*****
+*
+* Copyright (C) 2006 PreludeIDS Technologies. All Rights Reserved.
+* Author: Yoann Vandoorselaere <yoann.v@prelude-ids.com>
+*
+* This file is part of the Prelude-LML program.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by 
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING.  If not, write to
+* the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*****/
+
+#ifndef VALUE_CONTAINER_H
+#define VALUE_CONTAINER_H
 
 typedef struct value_container value_container_t;
 
