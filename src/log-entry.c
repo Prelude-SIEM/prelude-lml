a9310a0d21e28c529aa2213c033a83fc3c4bd550
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-09-12 09:09:10 +0000

    Use GnuLib time.h
    git-svn-id: file:///home/yoann/dev/prelude/git/nok/SVN/prelude-lml/trunk@10985 09c5ec92-17d4-0310-903a-819935f44dba

--- src/log-entry.c
+++ src/log-entry.c
@@ -37,17 +37,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <pcre.h>
-
-#if TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
-#endif
+#include <time.h>
 
 #include <libprelude/prelude-log.h>
 
