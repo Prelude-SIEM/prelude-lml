Prelude-SIEM/prelude-lml

commit ba3b3f87f169ce089e4598e26bfc8b8187a2e670

Initial TCP/TCP-TLS support, various improvement.
* TCP server support has been added and can be activated using the
tcp-server option, the default port is 514.

* TCP-TLS server support has been added and can be activated using the
"tcp-tls-server" option, the defaut port is 6514. The system support
anonymous authentication using the (anon-(EC)DH) key exchange protocol as
well as x509 authentication, which might be used with or without client
certificate verification ("tls-verify option"). Certificate verification
may include fingerprint (MD5 or SHA1) and/or name verification with the
use of the "tls-trusted-fingerprint" and "tls-trusted-name" option.

As part of TCP-TLS development, support for Diffie-Hellman parameters has
been implemented. The parameters are re-generated once a day, and default
to 1024 bits. This might be changed using the "dh-parameters-regenerate"
and "dh-prime-length" options.

Multiple TCP(/TLS) server might be configured, listening on different port
with different options.

The TCP(/TLS) implementation support the two RFC 6587 described method for
reading TCP syslog message :
 - Non transparent framing (raw message terminated with a TRAILER character).
 - Octet counting (message length explicitly indicated by the sender).

* Uniformisation of file length handling. Previously UDP syslog message were
limited to 1024 characters, and message read from file limited to 65535
characters. The input limit is now configurable through a command line and
a configuration option (log-max-length). The default limit has been adjusted
from 1024 to 8192 characters, which is the biggest value from the various
syslog RFC:
 - RFC 3164 (BSD Syslog protocol): packet MUST be 1024 bytes or less.
 - RFC 5424 (The Syslog protocol), section 6.1: messages of up to and
   including 2048 octets in length.  Transport receivers MAY receive
   messages larger than 2048 octets in length.
 - RFC 5425 (TLS Transport Mapping for Syslog), section 4.3.1: SHOULD

src/file-server.c
@@ -96,8 +96,6 @@
 #define METADATA_SIZE (sizeof(off_t) + METADATA_CHECKSUM_SIZE)
 #define METADATA_CHECKSUM_TYPE GCRY_MD_CRC32
 
-#define LOG_LINE_MAXSIZE 65535
-
 #define LOGFILE_RENAME_CLASS   "Log file rename"
 #define LOGFILE_DELETION_CLASS "Log file deletion"
 #define LOGFILE_DELETION_IMPACT "An attacker might have erased the logfile, "               \
@@ -319,17 +317,21 @@ static off_t read_logfile(monitor_fd_t *fd, off_t *available)
                 i++;
                 fd->current_line_len++;
 
-                if ( ret == '\n' )
+                if ( ret == '\n' ) {
+                        fd->current_line_len = 0;
                         break;
+                }
 
-                if ( fd->current_line_len <= LOG_LINE_MAXSIZE ) {
+                if ( fd->current_line_len <= config.log_max_length ) {
                         ret = prelude_string_ncat(fd->buf, (const char *) &ret, 1);
                         if ( ret < 0 )
                                 prelude_log(PRELUDE_LOG_ERR, "error buffering input: %s.\n", prelude_strerror(ret));
                 }
 
-                else if ( fd->current_line_len == LOG_LINE_MAXSIZE + 1 )
-                        prelude_log(PRELUDE_LOG_WARN, "line too long (internal limit of %u characters).\n", LOG_LINE_MAXSIZE);
+                else if ( fd->current_line_len == config.log_max_length + 1 ) {
+                        prelude_log(PRELUDE_LOG_WARN, "line too long (configured limit of %u characters).\n", config.log_max_length);
+                        fd->current_line_len = 0;
+                }
 
                 if ( i == *available ) {
                         *available = 0;
@@ -415,7 +417,7 @@ static int file_metadata_get_position(monitor_fd_t *monitor)
         struct stat st;
         const char *filename;
         int ret, have_metadata = 0;
-        off_t offset = 0, available = LOG_LINE_MAXSIZE;
+        off_t offset = 0, available = 65535;
         unsigned char msum[METADATA_SIZE], *sumptr = msum;
 
         filename = lml_log_source_get_name(monitor->source);
@@ -554,7 +556,7 @@ static int check_stdin_data(monitor_fd_t *monitor)
         int eventno = 0, bytes;
 
         if ( config.batch_mode )
-                len = LOG_LINE_MAXSIZE;
+                len = (off_t) config.log_max_length;
         else {
                 ret = ioctl(STDIN_FILENO, FIONREAD, &bytes);
                 if ( ret < 0 ) {

Makefile.am
@@ -27,6 +27,7 @@ MAINTAINERCLEANFILES = \
 
 install-data-local:
 	$(INSTALL) -m 700 -d $(DESTDIR)$(configdir);
+	$(INSTALL) -m 700 -d $(DESTDIR)$(lml_run_dir);
 	$(INSTALL) -m 700 -d $(DESTDIR)$(metadata_dir)
 	@if test -f $(DESTDIR)$(configdir)/prelude-lml.conf; then         					\
 		echo "********************************************************************************"; 	\

configure.in
@@ -103,6 +103,25 @@ AM_PATH_LIBPRELUDE(0.9.8, ,
 
 
 dnl **************************************************
+dnl * Check for GnuTLS.                              *
+dnl **************************************************
+GNUTLS_MIN_VERSION=1.0.17
+
+PKG_CHECK_MODULES([LIBGNUTLS], [gnutls >= $GNUTLS_MIN_VERSION], [],
+                  [AM_PATH_LIBGNUTLS($GNUTLS_MIN_VERSION, enable_gnutls=yes, enable_gnutls=no)])
+
+AC_CHECK_HEADER(gnutls/gnutls.h, enable_gnutls=yes, enable_gnutls=no)
+
+if test x$enable_gnutls = xyes; then
+        AC_DEFINE_UNQUOTED(HAVE_GNUTLS, , Tell whether GnuTLS is available for TCP-TLS support)
+	AC_CHECK_LIB(gnutls, gnutls_hash_get_len,
+             AC_DEFINE_UNQUOTED(HAVE_GNUTLS_HASH_GET_LEN, , Define whether GnuTLS provides gnutls_hash_get_len function,))
+fi
+
+AM_CONDITIONAL(HAVE_GNUTLS, test x$enable_gnutls = xyes)
+
+
+dnl **************************************************
 dnl * Check for libICU                               *
 dnl **************************************************
 
@@ -170,15 +189,18 @@ prelude_lml_conf=$configdir/prelude-lml.conf
 regex_conf=$configdir/plugins.rules
 metadata_dir=$LOCALSTATEDIR/lib/prelude-lml
 plugindir=$LIBDIR/prelude-lml
+lml_run_dir=$LOCALSTATEDIR/run/prelude-lml
 
 AC_DEFINE_UNQUOTED(PRELUDE_LML_CONF, "$prelude_lml_conf", Path to the LML configuration file)
 AC_DEFINE_UNQUOTED(LOG_PLUGIN_DIR, "$plugindir", Prelude-LML report plugin directory)
 AC_DEFINE_UNQUOTED(REGEX_CONF, "$regex_conf", Prelude-LML plugin regex configuration)
 AC_DEFINE_UNQUOTED(METADATA_DIR, "$metadata_dir", Prelude-LML Metadata directory)
+AC_DEFINE_UNQUOTED(LML_RUN_DIR, "$lml_run_dir", Prelude-LML run directory)
 
 AC_SUBST(configdir)
 AC_SUBST(metadata_dir)
 AC_SUBST(localstatedir)
+AC_SUBST(lml_run_dir)
 
 AC_SUBST(GLOBAL_CFLAGS)
 
@@ -211,4 +233,5 @@ AC_OUTPUT
 
 echo
 echo "*** Dumping configuration ***"
+echo "    - Enable TLS support         : $enable_gnutls"
 echo "    - Favor libICU over Iconv    : $enable_icu"

m4/libgnutls.m4
@@ -1,0 +1,160 @@
+dnl Autoconf macros for libgnutls
+dnl $id$
+
+# Modified for LIBGNUTLS -- nmav
+# Configure paths for LIBGCRYPT
+# Shamelessly stolen from the one of XDELTA by Owen Taylor
+# Werner Koch   99-12-09
+
+dnl AM_PATH_LIBGNUTLS([MINIMUM-VERSION, [ACTION-IF-FOUND [, ACTION-IF-NOT-FOUND ]]])
+dnl Test for libgnutls, and define LIBGNUTLS_CFLAGS and LIBGNUTLS_LIBS
+dnl
+AC_DEFUN([AM_PATH_LIBGNUTLS],
+[dnl
+dnl Get the cflags and libraries from the libgnutls-config script
+dnl
+AC_ARG_WITH(libgnutls-prefix,
+            AC_HELP_STRING(--with-libgnutls-prefix=PFX, [Prefix where libgnutls is installed (optional)]),
+          libgnutls_config_prefix="$withval", libgnutls_config_prefix="")
+
+  if test x$libgnutls_config_prefix != x ; then
+     if test x${LIBGNUTLS_CONFIG+set} != xset ; then
+        LIBGNUTLS_CONFIG=$libgnutls_config_prefix/bin/libgnutls-config
+     fi
+  fi
+
+  AC_PATH_PROG(LIBGNUTLS_CONFIG, libgnutls-config, no)
+  min_libgnutls_version=ifelse([$1], ,0.1.0,$1)
+  AC_MSG_CHECKING(for libgnutls - version >= $min_libgnutls_version)
+  no_libgnutls=""
+  if test "$LIBGNUTLS_CONFIG" = "no" ; then
+    no_libgnutls=yes
+  else
+    LIBGNUTLS_CFLAGS=`$LIBGNUTLS_CONFIG $libgnutls_config_args --cflags`
+    LIBGNUTLS_LIBS=`$LIBGNUTLS_CONFIG $libgnutls_config_args --libs`
+    libgnutls_config_version=`$LIBGNUTLS_CONFIG $libgnutls_config_args --version`
+
+
+      ac_save_CFLAGS="$CFLAGS"
+      ac_save_LIBS="$LIBS"
+      CFLAGS="$CFLAGS $LIBGNUTLS_CFLAGS"
+      LIBS="$LIBS $LIBGNUTLS_LIBS"
+dnl
+dnl Now check if the installed libgnutls is sufficiently new. Also sanity
+dnl checks the results of libgnutls-config to some extent
+dnl
+      rm -f conf.libgnutlstest
+      AC_TRY_RUN([
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gnutls/gnutls.h>
+
+int
+main ()
+{
+    system ("touch conf.libgnutlstest");
+
+    if( strcmp( gnutls_check_version(NULL), "$libgnutls_config_version" ) )
+    {
+      printf("\n*** 'libgnutls-config --version' returned %s, but LIBGNUTLS (%s)\n",
+             "$libgnutls_config_version", gnutls_check_version(NULL) );
+      printf("*** was found! If libgnutls-config was correct, then it is best\n");
+      printf("*** to remove the old version of LIBGNUTLS. You may also be able to fix the error\n");
+      printf("*** by modifying your LD_LIBRARY_PATH enviroment variable, or by editing\n");
+      printf("*** /etc/ld.so.conf. Make sure you have run ldconfig if that is\n");
+      printf("*** required on your system.\n");
+      printf("*** If libgnutls-config was wrong, set the environment variable LIBGNUTLS_CONFIG\n");
+      printf("*** to point to the correct copy of libgnutls-config, and remove the file config.cache\n");
+      printf("*** before re-running configure\n");
+    }
+    else if ( strcmp(gnutls_check_version(NULL), LIBGNUTLS_VERSION ) )
+    {
+      printf("\n*** LIBGNUTLS header file (version %s) does not match\n", LIBGNUTLS_VERSION);
+      printf("*** library (version %s)\n", gnutls_check_version(NULL) );
+    }
+    else
+    {
+      if ( gnutls_check_version( "$min_libgnutls_version" ) )
+      {
+        return 0;
+      }
+     else
+      {
+        printf("no\n*** An old version of LIBGNUTLS (%s) was found.\n",
+                gnutls_check_version(NULL) );
+        printf("*** You need a version of LIBGNUTLS newer than %s. The latest version of\n",
+               "$min_libgnutls_version" );
+        printf("*** LIBGNUTLS is always available from ftp://gnutls.hellug.gr/pub/gnutls.\n");
+        printf("*** \n");
+        printf("*** If you have already installed a sufficiently new version, this error\n");
+        printf("*** probably means that the wrong copy of the libgnutls-config shell script is\n");
+        printf("*** being found. The easiest way to fix this is to remove the old version\n");
+        printf("*** of LIBGNUTLS, but you can also set the LIBGNUTLS_CONFIG environment to point to the\n");
+        printf("*** correct copy of libgnutls-config. (In this case, you will have to\n");
+        printf("*** modify your LD_LIBRARY_PATH enviroment variable, or edit /etc/ld.so.conf\n");
+        printf("*** so that the correct libraries are found at run-time))\n");
+      }
+    }
+  return 1;
+}
+],, no_libgnutls=yes,[echo $ac_n "cross compiling; assumed OK... $ac_c"])
+       CFLAGS="$ac_save_CFLAGS"
+       LIBS="$ac_save_LIBS"
+  fi
+
+  if test "x$no_libgnutls" = x ; then
+     AC_MSG_RESULT(yes)
+     ifelse([$2], , :, [$2])
+  else
+     if test -f conf.libgnutlstest ; then
+        :
+     else
+        AC_MSG_RESULT(no)
+     fi
+     if test "$LIBGNUTLS_CONFIG" = "no" ; then
+       echo "*** The libgnutls-config script installed by LIBGNUTLS could not be found"
+       echo "*** If LIBGNUTLS was installed in PREFIX, make sure PREFIX/bin is in"
+       echo "*** your path, or set the LIBGNUTLS_CONFIG environment variable to the"
+       echo "*** full path to libgnutls-config."
+     else
+       if test -f conf.libgnutlstest ; then
+        :
+       else
+          echo "*** Could not run libgnutls test program, checking why..."
+          CFLAGS="$CFLAGS $LIBGNUTLS_CFLAGS"
+          LIBS="$LIBS $LIBGNUTLS_LIBS"
+          AC_TRY_LINK([
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gnutls/gnutls.h>
+],      [ return !!gnutls_check_version(NULL); ],
+        [ echo "*** The test program compiled, but did not run. This usually means"
+          echo "*** that the run-time linker is not finding LIBGNUTLS or finding the wrong"
+          echo "*** version of LIBGNUTLS. If it is not finding LIBGNUTLS, you'll need to set your"
+          echo "*** LD_LIBRARY_PATH environment variable, or edit /etc/ld.so.conf to point"
+          echo "*** to the installed location  Also, make sure you have run ldconfig if that"
+          echo "*** is required on your system"
+          echo "***"
+          echo "*** If you have an old version installed, it is best to remove it, although"
+          echo "*** you may also be able to get things to work by modifying LD_LIBRARY_PATH"
+          echo "***" ],
+        [ echo "*** The test program failed to compile or link. See the file config.log for the"
+          echo "*** exact error that occured. This usually means LIBGNUTLS was incorrectly installed"
+          echo "*** or that you have moved LIBGNUTLS since it was installed. In the latter case, you"
+          echo "*** may want to edit the libgnutls-config script: $LIBGNUTLS_CONFIG" ])
+          CFLAGS="$ac_save_CFLAGS"
+          LIBS="$ac_save_LIBS"
+       fi
+     fi
+     LIBGNUTLS_CFLAGS=""
+     LIBGNUTLS_LIBS=""
+     ifelse([$3], , :, [$3])
+  fi
+  rm -f conf.libgnutlstest
+  AC_SUBST(LIBGNUTLS_CFLAGS)
+  AC_SUBST(LIBGNUTLS_LIBS)
+])
+
+dnl *-*wedit:notab*-*  Please keep this as the last line.

prelude-lml.conf.in
@@ -95,6 +95,40 @@ prefix-regex = "^(?P<timestamp>.{15}) (?P<hostname>\S+) (?:(?P<process>\S+?)(?:\
 file = /var/log/messages
 file = /var/log/secure
 # udp-server = 0.0.0.0
+# tcp-server = 0.0.0.0
+# tcp-tls-server = 0.0.0.0
+
+# Two TLS mode are available :
+# x509 : x509 based certificate authentication
+# anonymous : Anonymous TLS connection using ANON-ECDH or ANON-DH
+#
+# The two mode can be combined, and the default value is x509.
+# tls-mode = x509 anonymous
+
+# Path for client key, certificate, and certificate authority files.
+# tls-key-file = /path/to/client.key
+# tls-cert-file = /path/to/client.cert
+# tls-ca-file = /path/to/server.ca
+
+# TLS verification option :
+#
+# When a list of trusted fingerprint and/or a list of trusted name is
+# provided, the connecting client have to match one of the provided
+# fingerprint/name, otherwise the connection will be refused.
+#
+# List of trusted client fingerprint (SHA1 and MD5 format accepted)
+# If this option is used, then the connecting peers have to match one of the specified fingerprint.
+# tls-trusted-fingerprint = SHA1:ae:8a:e5:97:66:2d:6d:cd:85:29:05:95:31:84:81:f1:27:3b:d1:73
+#
+# List of trusted client name (takes into account wildcards, and the
+# DNSName/IPAddress subject alternative name PKIX extension)
+# tls-trusted-name =
+#
+# Client certificate verification option (default is client-cert-required):
+# client-cert-required: client has to provide a valid certificate for the authentication to succeed.
+# client-cert-optional: client may connect without providing a valid x509 certificate.
+# tls-verify = client-cert-required
+
 
 
 #
@@ -104,7 +138,6 @@ file = /var/log/secure
 prefix-regex = "^(?P<timestamp>.{15}) \[(?P<process>\S+)\] "
 time-format = "%b %d %H:%M:%S"
 file = /var/log/everything/current
-# udp-server = 0.0.0.0
 
 
 #
@@ -170,4 +203,24 @@ ruleset=@configdir@/ruleset/pcre.rules
 #
 # Triger Report to the console.
 # stderr
+
+
+########################
+# Global TLS settings #
+#######################
+#
+# Number of bits for the prime used in the Diffie Hellman key exchange.
+# Note that the value should be one of 768, 1024, 2048, 3072 or 4096.
+# The default is 1024.
+#
+# dh-prime-length = 1024
+#
+# How often to regenerate the parameters used in the Diffie Hellman key
+# exchange. These should be discarded and regenerated once a day, once
+# a week or once a month, depending on the security requirements.
+#
+# Generation is a CPU intensive operation. The value is in hours,
+# 0 disables regeneration entirely. The default is 24 hours.
+#
+# dh-parameters-regenerate = 24
 

src/Makefile.am
@@ -9,8 +9,14 @@ prelude_lml_LDFLAGS = @LIBPRELUDE_LDFLAGS@ -export-dynamic \
         "-dlopen" $(top_builddir)/plugins/debug/debug.la \
         "-dlopen" $(top_builddir)/plugins/pcre/pcre.la
 
+if HAVE_GNUTLS
+ GNUTLS_DEPS = tcp-server-tls.c
+endif
+
 prelude_lml_SOURCES = 	\
         prelude-lml.c 	\
+        tcp-server.c    \
+        $(GNUTLS_DEPS)  \
         udp-server.c 	\
         regex.c 	\
         log-entry.c 	\

src/include/Makefile.am
@@ -9,6 +9,8 @@ noinst_HEADERS = 	\
 	log-source.h	\
 	log-plugins.h	\
         regex.h 	\
+        tcp-server.h    \
+        tcp-server-tls.h \
         udp-server.h 	\
         file-server.h 	
 

src/include/common.h
@@ -6,7 +6,7 @@
 * This file is part of the Prelude-LML program.
 *
 * This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by 
+* it under the terms of the GNU General Public License as published by
 * the Free Software Foundation; either version 2, or (at your option)
 * any later version.
 *
@@ -28,4 +28,4 @@
 
 void lml_dispatch_log(lml_log_source_t *ls, const char *str, size_t size);
 
-#endif				/* COMMON_H */
+#endif                          /* COMMON_H */

commit 2672a81733b3369113dbbc6c88e3437e0ee05714

Let the caller reset current_line_len
Commit ba3b3f87f169ce089e4598e26bfc8b8187a2e670 introduced a problem
where the metadata offset was not written correctly. As a result, LML
failed to match the metadata on restart, and started reading the log
file from the head position.

Change-Id: I030fa497bdd45e1b11ff29ad4843b4d06382059c

src/file-server.c
@@ -317,10 +317,8 @@ static off_t read_logfile(monitor_fd_t *fd, off_t *available)
                 i++;
                 fd->current_line_len++;
 
-                if ( ret == '\n' ) {
-                        fd->current_line_len = 0;
+                if ( ret == '\n' )
                         break;
-                }
 
                 if ( fd->current_line_len <= config.log_max_length ) {
                         ret = prelude_string_ncat(fd->buf, (const char *) &ret, 1);
@@ -328,10 +326,8 @@ static off_t read_logfile(monitor_fd_t *fd, off_t *available)
                                 prelude_log(PRELUDE_LOG_ERR, "error buffering input: %s.\n", prelude_strerror(ret));
                 }
 
-                else if ( fd->current_line_len == config.log_max_length + 1 ) {
+                else if ( fd->current_line_len == config.log_max_length + 1 )
                         prelude_log(PRELUDE_LOG_WARN, "line too long (configured limit of %u characters).\n", config.log_max_length);
-                        fd->current_line_len = 0;
-                }
 
                 if ( i == *available ) {
                         *available = 0;